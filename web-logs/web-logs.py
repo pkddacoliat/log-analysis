2f93b6ff57698214345306b8ffafe65650af0fbc
Patrick Kent Dacoliat <pkddacoliat@gmail.com>
2019-03-29 17:42:19 +0000

    added the web service that will output the alerts in a form of a table

---
+++ web-logs/web-logs.py
@@ -0,0 +1,27 @@
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
+
+import redis
+
+app = Flask(__name__)
+Bootstrap(app)
+
+@app.route("/")
+def index():
+    
+    alerts = []
+    try:
+        conn = redis.StrictRedis(host="redis", port=6379)
+        
+        for key in conn.scan_iter():
+            log = conn.get(key)
+            alerts.append(log)
+
+    except Exception as ex:
+        print(ex)
+
+    return render_template("index.html", alerts=alerts)
+
+
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0")
